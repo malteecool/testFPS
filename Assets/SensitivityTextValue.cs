81458736c56b8387ff6b89f3d54cac30fbf164d1
malte lindgren <malteecool@hotmail.com>
2021-06-12 16:37:16 +0200

    first commit

---
+++ Assets/SensitivityTextValue.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SensitivityTextValue : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+}
